e14a3813ff5689cac155af498ec3cb7b28319b1e
isinghpriya <isinghpriya2511@gmail.com>
2024-10-25 21:20:48 +0530

    hash password

---
+++ app/utils.py
@@ -0,0 +1,12 @@
+# app/utils.py
+
+from passlib.context import CryptContext
+
+# Use bcrypt for password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
