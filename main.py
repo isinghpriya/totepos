0e68d494cb940c3189252b65f8120d36e605c145
isinghpriya2511 <isinghpriya2511@gmail.com>
2024-10-27 15:07:41 +0530

    added routers for baskets

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 from fastapi import FastAPI
-from app.routers import employee,products
+from app.routers import employee,products,plugins, basket
 
 app = FastAPI()
 
 # Include routers
 app.include_router(employee.router, prefix="/employee")
 app.include_router(products.router, prefix="/products")
-# app.include_router(basket.router, prefix="/basket")
-# app.include_router(payment.router, prefix="/payment")
+app.include_router(plugins.router, prefix="/plugins")
+app.include_router(basket.router, prefix="/basket")
 
 @app.get("/")
 async def root():
